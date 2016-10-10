03a55f7fac7ecc65da3c5d7b3a70e38ca3251940
brian.marshall <brian.marshall@CDP-BRIANM.doshi.local>
2016-10-10 10:01:25 -0400

    started project

--- CUReportViewer/src/cureportviewer/CUReportViewer.java
+++ CUReportViewer/src/cureportviewer/CUReportViewer.java
@@ -1,13 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Front End for CU_Report Database
  */
 package cureportviewer;
 
 /**
  *
- * @author brian.marshall
+ * @author Brian
+ * @author Essa
  */
 public class CUReportViewer {
 
