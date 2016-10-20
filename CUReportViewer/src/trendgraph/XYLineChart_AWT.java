9f3e01606859c6807eb3f1c93f25cf7b61b6ec2a
eshomali <essa.shomali@CDP-ESSAS.doshi.local>
2016-10-20 12:52:10 -0400

    adjustments

--- CUReportViewer/src/trendgraph/XYLineChart_AWT.java
+++ CUReportViewer/src/trendgraph/XYLineChart_AWT.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package graphtest;
+package trendgraph;
 
 import java.awt.Color; 
 import java.awt.BasicStroke; 
@@ -26,50 +26,41 @@ public class XYLineChart_AWT extends ApplicationFrame
       super(applicationTitle);
       JFreeChart xylineChart = ChartFactory.createXYLineChart(
          chartTitle ,
-         "Category" ,
-         "Score" ,
+         "Year (YYYY)" , //X-axis
+         "Total Assets ($)" , //Y-axis
          createDataset() ,
          PlotOrientation.VERTICAL ,
          true , true , false);
          
       ChartPanel chartPanel = new ChartPanel( xylineChart );
-      chartPanel.setPreferredSize( new java.awt.Dimension( 560 , 367 ) );
+      chartPanel.setPreferredSize( new java.awt.Dimension( 1000 , 800 ) );   //(x, y)
       final XYPlot plot = xylineChart.getXYPlot( );
       XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer( );
-      renderer.setSeriesPaint( 0 , Color.RED );
-      renderer.setSeriesPaint( 1 , Color.GREEN );
-      renderer.setSeriesPaint( 2 , Color.YELLOW );
-      renderer.setSeriesStroke( 0 , new BasicStroke( 4.0f ) );
-      renderer.setSeriesStroke( 1 , new BasicStroke( 3.0f ) );
-      renderer.setSeriesStroke( 2 , new BasicStroke( 2.0f ) );
+      
+      renderer.setSeriesPaint( 0 , Color.RED ); //can be GREEN, YELLOW, ETC.
+
+      renderer.setSeriesStroke( 0 , new BasicStroke( 3.0f ) ); //Font size
+ 
       plot.setRenderer( renderer ); 
       setContentPane( chartPanel ); 
    }
    
    private XYDataset createDataset( )
    {
-      final XYSeries firefox = new XYSeries( "Firefox" );          
-      firefox.add( 1.0 , 1.0 );          
-      firefox.add( 2.0 , 4.0 );          
-      firefox.add( 3.0 , 3.0 );          
-      final XYSeries chrome = new XYSeries( "Chrome" );          
-      chrome.add( 1.0 , 4.0 );          
-      chrome.add( 2.0 , 5.0 );          
-      chrome.add( 3.0 , 6.0 );          
-      final XYSeries iexplorer = new XYSeries( "InternetExplorer" );          
-      iexplorer.add( 3.0 , 4.0 );          
-      iexplorer.add( 4.0 , 5.0 );          
-      iexplorer.add( 5.0 , 4.0 );          
+      final XYSeries cuName = new XYSeries( "CU Name" );          
+      cuName.add( 2004 , 1000 );    //can add more points if necessary                
+      cuName.add( 2016 , 10000 );          
+        
       final XYSeriesCollection dataset = new XYSeriesCollection( );          
-      dataset.addSeries( firefox );          
-      dataset.addSeries( chrome );          
-      dataset.addSeries( iexplorer );
+      
+      dataset.addSeries( cuName );          
+    
       return dataset;
    }
 
    public static void main( String[ ] args ) 
    {
-      XYLineChart_AWT chart = new XYLineChart_AWT("Browser Usage Statistics", "Which Browser are you using?");
+      XYLineChart_AWT chart = new XYLineChart_AWT("Year vs. Total Assets", "CU Report");
       chart.pack( );          
       RefineryUtilities.centerFrameOnScreen( chart );          
       chart.setVisible( true ); 
